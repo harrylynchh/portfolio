170585fa6feb95e404de871f7531b0a90cf30c7d
Harry Lynch <hlynch02@tufts.edu>
2024-11-14 17:10:26 -0500

    Added node and npm versions for Heroku

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import './App.css';
-import Footer from './sections/Footer/Footer';
 import Hero from './sections/Hero/Hero';
 import Projects from './sections/Projects/Projects';
-import Skills from './sections/Skills/Skills';
 import SkillsBalls from './sections/Skills/SkillsBalls';
 
 function App() {
