e3f6337139895cae84613e5a53df99a7a2f03e63
Harry Lynch <hlynch02@tufts.edu>
2025-01-26 16:30:03 -0500

    Added down arrow and changed projs

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -8,6 +8,8 @@ import emailLight from "../../assets/email-light.png";
 import emailDark from "../../assets/email-dark.png";
 import linkedinLight from "../../assets/linkedin-light.svg";
 import linkedinDark from "../../assets/linkedin-dark.svg";
+import downarrowLight from "../../assets/downarrow-light.png";
+import downarrowDark from "../../assets/downarrow-dark.png";
 import CV from "../../assets/harry_lynch_resume_1-6-2025.pdf";
 import { useTheme } from "../../common/ThemeContext";
 
@@ -18,55 +20,73 @@ function Hero() {
 	const emailIcon = theme === "light" ? emailLight : emailDark;
 	const githubIcon = theme === "light" ? githubLight : githubDark;
 	const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
+	const downArrow = theme === "light" ? downarrowLight : downarrowDark;
 
+	const navigateToProjects = () => {
+		const el = document.getElementById("projects");
+		if (el) {
+			el.scrollIntoView({ behavior: "smooth", block: "start" });
+		}
+	};
 	return (
-		<section id="hero" className={styles.container}>
-			<div className={styles.colorModeContainer}>
-				<img
-					src={heroImg}
-					className={styles.hero}
-					alt="Profile picture of Harris Johnsen"
-				/>
-				<img
-					className={styles.colorMode}
-					src={themeIcon}
-					alt="Color mode icon"
-					onClick={toggleTheme}
-				/>
-			</div>
-			<div className={styles.info}>
-				<h1>
-					Harry
-					<br />
-					Lynch
-				</h1>
-				<h2>Full Stack Developer</h2>
-				<span>
-					<a href="https://github.com/harrylynchh" target="_blank">
-						<img src={githubIcon} alt="Github icon" />
+		<section className={styles.heroWrapper}>
+			<section id="hero" className={styles.container}>
+				<div className={styles.colorModeContainer}>
+					<img
+						src={heroImg}
+						className={styles.hero}
+						alt="Profile picture of Harris Johnsen"
+					/>
+					<img
+						className={styles.colorMode}
+						src={themeIcon}
+						alt="Color mode icon"
+						onClick={toggleTheme}
+					/>
+				</div>
+				<div className={styles.info}>
+					<h1>
+						Harry
+						<br />
+						Lynch
+					</h1>
+					<h2>Full Stack Developer</h2>
+					<span>
+						<a
+							href="https://github.com/harrylynchh"
+							target="_blank"
+						>
+							<img src={githubIcon} alt="Github icon" />
+						</a>
+						<a href="mailto:hlynch02@tufts.edu" target="_blank">
+							<img src={emailIcon} alt="Email icon" />
+						</a>
+						<a
+							href="https://www.linkedin.com/in/harry-lynch-5350992a8/"
+							target="_blank"
+						>
+							<img src={linkedinIcon} alt="Linkedin icon" />
+						</a>
+					</span>
+					<p className={styles.description}>
+						Sophomore Computer Science Student at Tufts University
+						passionate about software development.
+						<br />
+						<br />
+						Please reach out through the above resources if you'd
+						like to connect!
+					</p>
+					<a href={CV} download>
+						<button className="hover">Résumé</button>
 					</a>
-					<a href="mailto:hlynch02@tufts.edu" target="_blank">
-						<img src={emailIcon} alt="Email icon" />
-					</a>
-					<a
-						href="https://www.linkedin.com/in/harry-lynch-5350992a8/"
-						target="_blank"
-					>
-						<img src={linkedinIcon} alt="Linkedin icon" />
-					</a>
-				</span>
-				<p className={styles.description}>
-					Sophomore Computer Science Student at Tufts University
-					passionate about software development.
-					<br />
-					<br />
-					Please reach out through the above resources if you'd like
-					to connect!
-				</p>
-				<a href={CV} download>
-					<button className="hover">Résumé</button>
-				</a>
-			</div>
+				</div>
+			</section>
+			<button
+				className={styles.downArrow}
+				onClick={() => navigateToProjects()}
+			>
+				<img width="50px" src={downArrow} alt="Down Arrow"></img>
+			</button>
 		</section>
 	);
 }
--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,38 +1,38 @@
-import styles from './ProjectsStyles.module.css';
-import portfolio from '../../assets/portfolio.png';
-import airtight from '../../assets/airtight.png';
-import cpp from '../../assets/cppIcon.png';
-import ProjectCard from '../../common/ProjectCard';
+import styles from "./ProjectsStyles.module.css";
+import portfolio from "../../assets/portfolio.png";
+import airtight from "../../assets/airtight.png";
+import cpp from "../../assets/cppIcon.png";
+import ProjectCard from "../../common/ProjectCard";
 
 function Projects() {
-  return (
-    <section id="projects" className={styles.container}>
-      <h1 className="sectionTitle">Projects</h1>
-      <div className={styles.projectsContainer}>
-        <ProjectCard
-          src={portfolio}
-          link="https://github.com/harrylynchh/portfolio"
-          h3="My Portfolio Site"
-          p="This Portfolio Site with React, 3JS, and Node"
-        />
+	return (
+		<section id="projects" className={styles.container}>
+			<h1 className="sectionTitle">Projects</h1>
+			<div className={styles.projectsContainer}>
+				<ProjectCard
+					src={portfolio}
+					link="https://github.com/harrylynchh/portfolio"
+					h3="My Portfolio Site"
+					p="This Portfolio Site with React, 3JS, and Node"
+				/>
 
-        <ProjectCard
-          src={airtight}
-          link="https://github.com/harrylynchh/airtight-container"
-          h3="Container Inventory"
-          p="Web-Based Container Software with PERN Stack"
-          gold={true}
-        />
-        
-        <ProjectCard
-          src={cpp}
-          link="https://github.com/harrylynchh/cppProjectDetails"
-          h3="C++ Projects"
-          p="Projects in Data Structures & Algorithms."
-        />
-      </div>
-    </section>
-  );
+				<ProjectCard
+					src={airtight}
+					link="https://github.com/harrylynchh/airtight-container"
+					h3="Container Inventory"
+					p="Web-Based Container Software with PERN Stack"
+					gold={true}
+				/>
+
+				<ProjectCard
+					src={cpp}
+					link="https://github.com/harrylynchh/c-cppProjectDetails"
+					h3="C/C++ Projects"
+					p="Projects in Data Structures & Algorithms."
+				/>
+			</div>
+		</section>
+	);
 }
 
 export default Projects;
