e18c15ca0eb1e6b40fdac6db824d07358eaa71ff
Harry Lynch <hlynch02@tufts.edu>
2025-02-02 14:05:53 -0500

    Updated resume

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -10,7 +10,7 @@ import linkedinLight from "../../assets/linkedin-light.svg";
 import linkedinDark from "../../assets/linkedin-dark.svg";
 import downarrowLight from "../../assets/downarrow-light.png";
 import downarrowDark from "../../assets/downarrow-dark.png";
-import CV from "../../assets/harry_lynch_resume_1-6-2025.pdf";
+import CV from "../../assets/harry_lynch_resume_2-2-2025.pdf";
 import { useTheme } from "../../common/ThemeContext";
 
 function Hero() {
